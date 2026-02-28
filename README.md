50998c929ce121f063b994889e909b7efc8151a9
Mykyta Nuzhnyi <nuznhy@gmail.com>
2026-02-28 14:31:27 +0200

    updated markdown files and added readme

--- README.md
+++ README.md
@@ -12,78 +12,18 @@
 > [!WARNING]
 > Make your config backup just in case
 >``` sh 
->   mv ~/.config/nvim{,.bak}
+> mv ~/.config/nvim{,.bak}
 >```
 
 > [!TIP]
 > Make your nvim backup just in case
 >``` sh
->    mv ~/.local/share/nvim{,.bak}
->    mv ~/.local/state/nvim{,.bak}
->    mv ~/.cache/nvim{,.bak}
+> mv ~/.local/share/nvim{,.bak}
+> mv ~/.local/state/nvim{,.bak}
+> mv ~/.cache/nvim{,.bak}
 >```
 
 > Clone config to ~/.config/nvim
 >``` sh
 > git clone https://github.com/Nuznhy/nvim-config.git ~/.config/nvim
->```
-
-> [!WARNING]
-> If you are not using [noctalia-shell](https://noctalia.dev/), comment this section in nvim/lua/config/autocmd.lua and change colorscheme manually. This autocmd is needed to autochange theme when noctalia-shell changed dark/light modes
->``` lua 
->   -- sync theme with noctalia-shell --
->   local function get_noctalia_dark_mode()
->     local result = vim.system(
->       { "qs", "-c", "noctalia-shell", "ipc", "call", "state", "all" },
->       { text = true }
->     ):wait()
->   
->     if result.code ~= 0 then
->       return nil
->     end
->   
->     local ok, decoded = pcall(vim.json.decode, result.stdout)
->     if not ok then
->       return nil
->     end
->   
->     return decoded.settings.colorSchemes.darkMode
->   end
->   
->   local function apply_rose_pine_from_noctalia()
->     local is_dark = get_noctalia_dark_mode()
->   
->     if is_dark == nil then
->       return
->     end
->   
->     local variant = is_dark and "main" or "dawn"
->   
->     require("rose-pine").setup({
->       variant = variant,
->       dark_variant = "main",
->       styles = {
->         transparency = is_dark, -- example behavior
->       },
->     })
->   
->     vim.cmd("colorscheme rose-pine")
->     vim.cmd("hi statusline guibg=NONE")
->   end
->   vim.g.rose_pine_variant = "main"
->   require("rose-pine").setup({
->       variant = vim.g.rose_pine_variant,
->       dark_variant = "main",
->       styles = {
->           transparency = true,
->       },
->   })
->   
->   apply_rose_pine_from_noctalia()
->   local signal = vim.uv.new_signal()
->   
->   signal:start("sigusr1", vim.schedule_wrap(function()
->     apply_rose_pine_from_noctalia()
->   end))
->   --
 >```
--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,72 +1,77 @@
 -- sync theme with noctalia-shell --
-local function get_noctalia_dark_mode()
-  local result = vim.system(
-    { "qs", "-c", "noctalia-shell", "ipc", "call", "state", "all" },
-    { text = true }
-  ):wait()
+if vim.fn.executable("qs") == 1 then
+    local function get_noctalia_dark_mode()
+        local result = vim.system(
+            { "qs", "-c", "noctalia-shell", "ipc", "call", "state", "all" },
+            { text = true }
+        ):wait()
 
-  if result.code ~= 0 then
-    return nil
-  end
+        if result.code ~= 0 then
+            return nil
+        end
 
-  local ok, decoded = pcall(vim.json.decode, result.stdout)
-  if not ok then
-    return nil
-  end
+        local ok, decoded = pcall(vim.json.decode, result.stdout)
+        if not ok then
+            return nil
+        end
 
-  return decoded.settings.colorSchemes.darkMode
+        return decoded.settings.colorSchemes.darkMode
+    end
+
+    local function apply_rose_pine_from_noctalia()
+        local is_dark = get_noctalia_dark_mode()
+
+        if is_dark == nil then
+            return
+        end
+
+        local variant = is_dark and "main" or "dawn"
+
+        require("rose-pine").setup({
+            variant = variant,
+            dark_variant = "main",
+            styles = {
+                transparency = is_dark, -- example behavior
+            },
+        })
+
+        vim.cmd("colorscheme rose-pine")
+        vim.cmd("hi statusline guibg=NONE")
+    end
+    vim.g.rose_pine_variant = "main"
+    require("rose-pine").setup({
+        variant = vim.g.rose_pine_variant,
+        dark_variant = "main",
+        styles = {
+            transparency = true,
+        },
+    })
+
+    apply_rose_pine_from_noctalia()
+    local signal = vim.uv.new_signal()
+
+    signal:start("sigusr1", vim.schedule_wrap(function()
+        apply_rose_pine_from_noctalia()
+    end))
+    --
+else
+    require("rose-pine").setup({ variant = "main", dark_variant = "main" })
+    vim.cmd("colorscheme rose-pine")
 end
-
-local function apply_rose_pine_from_noctalia()
-  local is_dark = get_noctalia_dark_mode()
-
-  if is_dark == nil then
-    return
-  end
-
-  local variant = is_dark and "main" or "dawn"
-
-  require("rose-pine").setup({
-    variant = variant,
-    dark_variant = "main",
-    styles = {
-      transparency = is_dark, -- example behavior
-    },
-  })
-
-  vim.cmd("colorscheme rose-pine")
-  vim.cmd("hi statusline guibg=NONE")
-end
-vim.g.rose_pine_variant = "main"
-require("rose-pine").setup({
-    variant = vim.g.rose_pine_variant,
-    dark_variant = "main",
-    styles = {
-        transparency = true,
-    },
-})
-
-apply_rose_pine_from_noctalia()
-local signal = vim.uv.new_signal()
-
-signal:start("sigusr1", vim.schedule_wrap(function()
-  apply_rose_pine_from_noctalia()
-end))
---
 
 -- move help windows to Vertical split --
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "help",
-  callback = function()
-    vim.cmd("wincmd L")
-  end,
+    pattern = "help",
+    callback = function()
+        vim.cmd("wincmd L")
+    end,
 })
 
 
 -- autoformat on save --
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
+    pattern = "*",
+    callback = function(args)
+        require("conform").format({ bufnr = args.buf })
+    end,
 })
