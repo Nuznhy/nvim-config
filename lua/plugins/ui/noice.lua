c53da3d08682f427f836513455958a3faa62d05c
nuznhy <nuznhy@gmail.com>
2026-05-18 02:39:22 +0300

    fix nvim bg

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -1,11 +1,80 @@
+local function transparent_noice()
+  local groups = {
+    "NoiceCmdlinePopup",
+    "NoiceCmdlinePopupBorder",
+    "NoiceCmdlinePopupBorderCmdline",
+    "NoiceCmdlinePopupBorderSearch",
+    "NoiceCmdlinePopupBorderHelp",
+    "NoiceCmdlinePopupBorderLua",
+    "NoiceCmdlinePopupBorderInput",
+    "NoiceCmdlinePopupTitle",
+  }
+
+  for _, group in ipairs(groups) do
+    vim.api.nvim_set_hl(0, group, { bg = "NONE" })
+  end
+end
+
+transparent_noice()
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = transparent_noice,
+})
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-        -- add any options here
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            views = {
+                cmdline_popup = {
+                    win_options = {
+                        winhighlight = {
+                            Normal = "NormalFloat",
+                            FloatBorder = "FloatBorder",
+                            FloatTitle = "FloatTitle",
+                        },
+                    },
+                },
+
+                popupmenu = {
+                    win_options = {
+                        winhighlight = {
+                            Normal = "Pmenu",
+                            FloatBorder = "FloatBorder",
+                            CursorLine = "PmenuSel",
+                        },
+                    },
+                },
+
+                confirm = {
+                    win_options = {
+                        winhighlight = {
+                            Normal = "NormalFloat",
+                            FloatBorder = "FloatBorder",
+                        },
+                    },
+                },
+
+                hover = {
+                    win_options = {
+                        winhighlight = {
+                            Normal = "NormalFloat",
+                            FloatBorder = "FloatBorder",
+                        },
+                    },
+                },
+            },
+        },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
     },
-    dependencies = {
-        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-        "MunifTanjim/nui.nvim",
-    }
+    {
+        "rcarriga/nvim-notify",
+        opts = {
+            background_colour = "#000000",
+        }
+    },
 }
