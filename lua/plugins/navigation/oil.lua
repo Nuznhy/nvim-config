bdb2bf6b95e565f4f1566859406492b759dbffcc
nuznhy <nuznhy@gmail.com>
2026-06-05 13:39:56 +0300

    added oil preview

--- lua/plugins/navigation/oil.lua
+++ lua/plugins/navigation/oil.lua
@@ -14,6 +14,12 @@ return {
             --"size",
             --"mtime",
         },
+        preview_win = {
+            update_on_cursor_moved = true,
+            preview_method = "fast_scratch",
+        },
+
+        preview_split = "right",
         -- git = {
         --     -- Return true to automatically git add/mv/rm files
         --     add = function(path)
@@ -27,6 +33,23 @@ return {
         --     end,
         -- },
     },
+    config = function(_, opts)
+        require("oil").setup(opts)
+
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "OilEnter",
+            callback = vim.schedule_wrap(function(args)
+                local oil = require("oil")
+
+                if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+                    oil.open_preview({
+                        vertical = true,
+                        split = "belowright"
+                    })
+                end
+            end),
+        })
+    end,
     -- Optional dependencies
     dependencies = { { "nvim-tree/nvim-web-devicons" } }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
