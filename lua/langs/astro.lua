d42d1c1ccb0c1bd83bac55f12372a9a9d6806b15
nuznhy <nuznhy@gmail.com>
2026-09-02 19:58:00 +0300

    removed astro

--- lua/langs/astro.lua
+++ lua/langs/astro.lua
@@ -1,25 +1,25 @@
-vim.filetype.add({
-  extension = {
-    astro = "astro",
-  },
-})
-
-vim.lsp.config("astro", {
-  cmd = { "astro-ls", "--stdio" },
-  filetypes = { "astro" },
-  root_markers = {
-    "package.json",
-    "astro.config.mjs",
-    "astro.config.ts",
-    "tsconfig.json",
-    "jsconfig.json",
-    ".git",
-  },
-})
-
-vim.lsp.enable("astro")
-
-return {
-  servers = { "astro" },
-  parsers = { "astro" },
-}
+-- vim.filetype.add({
+--   extension = {
+--     astro = "astro",
+--   },
+-- })
+--
+-- vim.lsp.config("astro", {
+--   cmd = { "astro-ls", "--stdio" },
+--   filetypes = { "astro" },
+--   root_markers = {
+--     "package.json",
+--     "astro.config.mjs",
+--     "astro.config.ts",
+--     "tsconfig.json",
+--     "jsconfig.json",
+--     ".git",
+--   },
+-- })
+--
+-- vim.lsp.enable("astro")
+--
+-- return {
+--   servers = { "astro" },
+--   parsers = { "astro" },
+-- }
